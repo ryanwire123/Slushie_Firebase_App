01bf4b7633c975bc6300c825a40d04e36cac78bb
Matthew LaPorta <matthewlaporta43@gmail.com>
2024-12-04 16:27:48 -0500

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,16 @@
+// src/components/Header.jsx
+
 import React from 'react';
 import './Header.css'; // Add the CSS styling for the header
+import logo from '../assets/whiteLogo250PxV2.png';
 
 const Header = () => {
   return (
     <header className="app-header">
+    <header className="app-header2"></header>
       <div className="header-content">
-        <img src="your-logo-url.png" alt="App Logo" className="app-logo" />
-        <h1 className="app-title">Slushies Ahoy!</h1>
+        <img src={logo} alt="App Logo" className="app-logo" />
+        <h1 className="app-title">Slushies Ahoy</h1>
       </div>
     </header>
   );
